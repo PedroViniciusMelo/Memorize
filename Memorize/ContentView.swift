9d02e431819496e23e9d396d060d743b26506646
PedroViniciusMelo <pedro.vinicius.melo.silva@gmail.com>
2024-02-06 11:32:13 -0300

    Added CardView component

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,13 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+           CardView(isFaceUp: true)
+           CardView(isFaceUp: false)
+           CardView(isFaceUp: true)
+           CardView(isFaceUp: false)
         }
+        .foregroundColor(.orange)
         .padding()
+    }
+}
+
+struct CardView : View {
+    var isFaceUp : Bool = false
+    var body: some View {
+        if isFaceUp {
+            ZStack {
+                RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+                    .strokeBorder(lineWidth: 2)
+                Text("👻").font(.largeTitle)
+            }
+        } else {
+            RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+                .foregroundColor(.orange)
+            
+        }
     }
 }
 
