ebe724bcbc0231cd2a5c6ecdcd90290901ca0637
PedroViniciusMelo <pedro.vinicius.melo.silva@gmail.com>
2024-02-07 10:29:22 -0300

    Added CardView list

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,33 +8,80 @@
 import SwiftUI
 
 struct ContentView: View {
+    let emojis = ["👻", "🕷️", "🎃", "😈", "👹"]
+    @State var emojisCount = 4
+    
     var body: some View {
-        HStack {
-           CardView(isFaceUp: true)
-           CardView(isFaceUp: false)
-           CardView(isFaceUp: true)
-           CardView(isFaceUp: false)
+        VStack {
+            ScrollView {
+                cards
+            }
+            
+            Spacer()
+            
+            cardCountAdjusters
+        }.padding()
+        
+    }
+    
+    
+    var cards : some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+            ForEach(0..<emojisCount, id: \.self) { index in
+                CardView(emoji : emojis[index])
+                    .aspectRatio(2/3, contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+            }
         }
         .foregroundColor(.orange)
-        .padding()
+    }
+    
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
+        Button (
+            action: {
+                emojisCount += offset
+            }, label: {
+                Image(systemName: symbol)
+            }
+        ).disabled(emojisCount + offset < 1 || emojisCount + offset > emojis.count )
+    }
+    
+    var cardRemover : some View {
+        cardCountAdjuster(by: -1, symbol: "rectangle.stack.fill.badge.minus")
+    }
+    
+    var cardAdder : some View {
+        cardCountAdjuster(by: +1, symbol: "rectangle.stack.fill.badge.plus")
+    }
+    
+    var cardCountAdjusters : some View {
+        HStack {
+            cardRemover
+            
+            Spacer()
+            
+            cardAdder
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
     }
 }
 
 struct CardView : View {
-    var isFaceUp : Bool = false
+    let emoji : String
+    @State var isFaceUp = false
+    
     var body: some View {
-        if isFaceUp {
-            ZStack {
-                RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
-                    .foregroundColor(.white)
-                RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
-                    .strokeBorder(lineWidth: 2)
-                Text("👻").font(.largeTitle)
-            }
-        } else {
-            RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
-                .foregroundColor(.orange)
-            
+        let base = RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Group {
+                base.foregroundColor(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text(emoji).font(.largeTitle)
+            }.opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
         }
     }
 }
